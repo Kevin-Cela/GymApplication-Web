5006ebdffa946ffbb2cf2a535487ea36700f687d
Kevin Cela <kcela21@epoka.edu.al>
2023-06-07 18:35:58 +0200

    Fixed Error when registering
    - Session failed to start correctly when registering
    - Refactored session code so that it is more convinient to use

--- components/Validation/Authenthicate.php
+++ components/Validation/Authenthicate.php
@@ -6,51 +6,52 @@ require('backend/models/manager.php');
 
 function authenthicate($email, $password, $database)
 {
-  $stm = $database->connection->prepare("SELECT * FROM members WHERE password = (?) AND email = (?)");
-  $stm->bind_param("ss", $password, $email);
-  $stm->execute();
-  $res = $stm->get_result();
-  $row = $res->fetch_assoc();
-
-  if ($row != null) {
-    session_start();
-    $member = new Member($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
-    $_SESSION['user'] = $member;
-    $_SESSION['role'] = 'MEMBER';
-    header('Location: components/Validation/test.php');
-    exit();
-  } else {
-    $stm = $database->connection->prepare("SELECT * FROM coaches WHERE password = (?) AND email = (?)");
+    $stm = $database->connection->prepare("SELECT * FROM members WHERE password = (?) AND email = (?)");
     $stm->bind_param("ss", $password, $email);
     $stm->execute();
     $res = $stm->get_result();
     $row = $res->fetch_assoc();
+
     if ($row != null) {
-      session_start();
-      $coach = new Coach($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
-      $_SESSION['user'] = $coach;
-      $_SESSION['role'] = 'COACH';
-      header('Location: components/Validation/test.php');
-      exit();
-    } else {
-      $stm = $database->connection->prepare("SELECT * FROM managers WHERE password = (?) AND email = (?)");
-      $stm->bind_param("ss", $password, $email);
-      $stm->execute();
-      $res = $stm->get_result();
-      $row = $res->fetch_assoc();
-      if ($row != null) {
         session_start();
-        $manager = new Manager($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
-        $_SESSION['user'] = $manager;
-        $_SESSION['role'] = 'MANAGER';
-        header('Location: components/Validation/test.php');
+        $member = new Member($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
+        $_SESSION['user'] = $member;
+        $_SESSION['role'] = 'MEMBER';
+        header('Location: index.php');
         exit();
-      }
+    } else {
+        $stm = $database->connection->prepare("SELECT * FROM coaches WHERE password = (?) AND email = (?)");
+        $stm->bind_param("ss", $password, $email);
+        $stm->execute();
+        $res = $stm->get_result();
+        $row = $res->fetch_assoc();
+        if ($row != null) {
+            session_start();
+            $coach = new Coach($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
+            $_SESSION['user'] = $coach;
+            $_SESSION['role'] = 'COACH';
+            header('Location: index.php');
+            exit();
+        } else {
+            $stm = $database->connection->prepare("SELECT * FROM managers WHERE password = (?) AND email = (?)");
+            $stm->bind_param("ss", $password, $email);
+            $stm->execute();
+            $res = $stm->get_result();
+            $row = $res->fetch_assoc();
+            if ($row != null) {
+                session_start();
+                $manager = new Manager($row['id'], $row['password'], $row['first_name'], $row['last_name'], $row['email'], $row['phone_number']);
+                $_SESSION['user'] = $manager;
+                $_SESSION['role'] = 'MANAGER';
+                header('Location: index.php');
+                exit();
+            }
+        }
     }
-  }
 }
 
-function isEmailInUse($email, $database){
+function isEmailInUse($email, $database)
+{
     $stm1 = $database->connection->prepare("SELECT email FROM `members` WHERE email=(?)");
     $stm1->bind_param("s", $email);
     $stm1->execute();
@@ -69,28 +70,21 @@ function isEmailInUse($email, $database){
     $res3 = $stm3->get_result();
     $row3 = $res3->fetch_assoc();
 
-    if($row1 == null && $row2 == null && $row3 == null){
+    if ($row1 == null && $row2 == null && $row3 == null) {
         return false;
-    }
-    else{
+    } else {
         return true;
     }
 }
 
 function register($fname, $lname, $phone, $email, $password, $database)
 {
-  $stm = $database->connection->prepare("INSERT INTO members (password, first_name, last_name, email, phone_number) VALUES (?,?,?,?,?)");
+    $stm = $database->connection->prepare("INSERT INTO members (password, first_name, last_name, email, phone_number) VALUES (?,?,?,?,?)");
 
-  $stm->bind_param("sssss", $password, $fname, $lname, $email, $phone);
-  $stm->execute();
-  $res = $stm->get_result();
+    $stm->bind_param("sssss", $password, $fname, $lname, $email, $phone);
+    $stm->execute();
+    $res = $stm->get_result();
 
-  session_start();
-  $a = $fname;
-  $a .= " ";
-  $a .= $lname;
-  $_SESSION['username'] = $a;
-  header('Location: components/Validation/test.php');
-  exit();
+    authenthicate($email, $password, $database);
 }
-
+?>
